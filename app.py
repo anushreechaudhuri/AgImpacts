540292df908854affba525bbc3cc6243035ded38
anushreechaudhuri <anuc@mit.edu>
2021-01-14 16:26:36 -0800

    added raw data, changed to side bar, starting to implement GHG --> impact tool

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(layout='wide')
 import plotly.express as px
 
-st.title('AgImpacts Interactive Web Tool')
+st.title('Welcome to the AgImpacts Interactive Web Tool!')
 
 
 @st.cache(show_spinner=False, persist=True)  # save computation between reruns
@@ -26,7 +26,7 @@ commodity_rows = {
 }
 
 # Select the commodity to analyze
-commodity = st.selectbox(label='Commodity', options=list(commodity_rows.keys()))
+commodity = st.sidebar.selectbox(label='Commodity', options=list(commodity_rows.keys()))
 start, end = commodity_rows[commodity]
 df_filtered = full_df[start:end].dropna(axis=0, subset=['Reference'])
 
@@ -39,7 +39,10 @@ col_labels = [  # Tuples of (column name, pretty print name for axis labels)
 ('Eutrophying Emissions (kg PO4 eq)', 'Eutrophication Potential (kg PO<sub>4</sub><sup>3-</sup> eq)'),
 ('Acidifying Emissions (kg SO2 eq)', 'Acidification Potential (kg SO<sub>2</sub> eq)')]
 
-with st.beta_expander('Country Analysis'):
+indicators = ['Land Use (m^2*year)', 'Freshwater Withdrawal (L)', 
+'Acidifying Emissions (kg SO2 eq)', 'Eutrophying Emissions (kg PO4 eq)']
+
+with st.beta_expander('Geographic Overview of Indicators'):
     st.markdown('This tool analyzes a column as a function of the country.')
     col = st.selectbox(label='Column to Analyze', options=numerical_cols)
 
@@ -54,3 +57,31 @@ with st.beta_expander('Country Analysis'):
                          title=f'{col} for {commodity}', labels={'locations': 'Country', 'size': col}, width=1500,
                          height=600)
     st.plotly_chart(fig)
+with st.beta_expander('See Raw Data'):
+    st.dataframe(df_filtered)
+
+ghg = 'GHG Emissions (kg CO2 eq)'
+with st.beta_expander('Impact Analysis (Work in Progress)'):
+    quantile_list = df_filtered[ghg].quantile([0.25,0.5,0.75, 1.0])
+    quantile_list = pd.DataFrame(quantile_list)
+    quartile_titles = ['Q1', 'Q2', 'Q3','Q4']
+    quantile_list['Quart Titles'] = quartile_titles
+    st.write(quantile_list)
+    q1 = st.checkbox('Quartile 1 of Producer GHG Emissions (0-25%)')
+    q2 = st.checkbox('Quartile 2 of Producer GHG Emissions (25-50%)')
+    q3 = st.checkbox('Quartile 3 of Producer GHG Emissions (50-75%)')
+    q3 = st.checkbox('Quartile 4 of Producer GHG Emissions (75-100%)')
+    if q1:
+        for indicator in indicators:
+            y_axis = [indicator]
+            df_filtered[indicator] = df_filtered[indicator].replace(',', '', regex=True).replace('-', '', regex=True).replace('%', '', regex=True).replace(' ', '', regex=True).replace('', 'NaN').astype(float)  # remove commas
+            average = [df_filtered[indicator].mean()]
+            fig = px.bar(x=average, y=y_axis,
+                 labels={'x': 'Value'}, template = 'simple_white', orientation='h')
+            st.plotly_chart(fig)
+
+
+
+    
+
+    
