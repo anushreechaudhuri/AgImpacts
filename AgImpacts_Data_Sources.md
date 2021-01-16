e5f9582cf46272d85b92cd9b6a87b6d0e927b79b
anushreechaudhuri <anuc@mit.edu>
2021-01-15 16:55:15 -0800

    fixed for all commodities, added new features

---
+++ AgImpacts_Data_Sources.md
@@ -0,0 +1,41 @@
+# AgImpacts Data Sources
+The main dataset used for this project is from the paper [_Reducing food’s environmental impacts through producers and consumers_](https://science.sciencemag.org/content/360/6392/987/tab-figures-data). 
+
+---
+For salmon, the following additional sources were used:
+
+https://www.sciencedirect.com/science/article/abs/pii/S0959652608001820
+https://link.springer.com/article/10.1007/s11367-017-1340-9
+https://pubs.acs.org/doi/10.1021/es9010114
+
+---
+
+For shrimp, the following additional sources were used: 
+
+https://pubs.acs.org/doi/10.1021/es104058z 
+http://dx.doi.org/10.1016/j.aquaculture.2014.09.019
+
+----
+
+For roundwood, the following additional sources were used: 
+
+https://www.sciencedirect.com/science/article/abs/pii/S0959652613003193 
+https://www.sciencedirect.com/science/article/pii/S0048969718319144 
+https://link.springer.com/article/10.1007/s00107-020-01544-7 
+https://www.sciencedirect.com/science/article/pii/S0959652619323376 
+https://www.sciencedirect.com/science/article/pii/S0048969718324628 
+https://www.sciencedirect.com/science/article/abs/pii/S0048969713013156 
+https://www.sciencedirect.com/science/article/pii/S0959652618304931 
+https://www.sciencedirect.com/science/article/abs/pii/S0959652612004027 
+https://corrim.org/wp-content/uploads/2018/06/SE-Lumber-LCA-may-2013-final.pdf 
+https://www.researchgate.net/publication/263227467_Cradle-to-gate_life_cycle_assessment_of_softwood_lumber_production_from_the_northeast-north_central 
+https://link.springer.com/article/10.1007%2Fs11367-013-0583-3 
+https://www.tandfonline.com/doi/abs/10.1080/10549811.2020.1768871?journalCode=wjsf20 
+https://www.sciencedirect.com/science/article/pii/S0959652615002127 
+https://link.springer.com/article/10.1007/s11367-009-0089-1
+
+---
+
+For tuna, the following additional sources were used:
+
+https://link.springer.com/article/10.1007/s11367-015-0943-2[ https://www.sciencedirect.com/science/article/abs/pii/S016578360500175X](https://www.sciencedirect.com/science/article/abs/pii/S016578360500175X)[ https://www.sciencedirect.com/science/article/pii/S0959652614004776?via%3Dihub](https://www.sciencedirect.com/science/article/pii/S0959652614004776?via%3Dihub)[ https://iss-foundation.org/download-monitor-demo/download-info/issf-technical-report-2012-03-fuel-consumption-and-greenhouse-gas-emissions-from-global-tuna-fisheries-a-preliminary-assessment/](https://iss-foundation.org/download-monitor-demo/download-info/issf-technical-report-2012-03-fuel-consumption-and-greenhouse-gas-emissions-from-global-tuna-fisheries-a-preliminary-assessment/)[https://www.mcst-rmiusp.org/index.php/resources/reference-library/download/6-fisheries-fuel-and-emissions/560-world-bank-asian-development-bank-2009-study-of-the-impact-of-energy-price-fluctuations-on-fisheries-in-the-pacific-with-emphasis-on-the-tuna-industry-final-report](https://www.mcst-rmiusp.org/index.php/resources/reference-library/download/6-fisheries-fuel-and-emissions/560-world-bank-asian-development-bank-2009-study-of-the-impact-of-energy-price-fluctuations-on-fisheries-in-the-pacific-with-emphasis-on-the-tuna-industry-final-report)[ https://www.researchgate.net/publication/237712293_Estimating_the_Carbon_Footprint_of_Tuna_Fisheries](https://www.researchgate.net/publication/237712293_Estimating_the_Carbon_Footprint_of_Tuna_Fisheries)[ https://onlinelibrary.wiley.com/doi/abs/10.1002/pts.975](https://onlinelibrary.wiley.com/doi/abs/10.1002/pts.975)[ https://www.sciencedirect.com/science/article/pii/S0921344905001515](https://www.sciencedirect.com/science/article/pii/S0921344905001515)[ https://esajournals.onlinelibrary.wiley.com/doi/abs/10.1002/fee.1822](https://esajournals.onlinelibrary.wiley.com/doi/abs/10.1002/fee.1822)[ https://www.jstage.jst.go.jp/article/suisan/70/4/70_4_548/_article](https://www.jstage.jst.go.jp/article/suisan/70/4/70_4_548/_article)
--- app.py
+++ app.py
@@ -5,18 +5,32 @@ st.set_page_config(layout='wide')
 import plotly.express as px
 
 st.title('Welcome to the AgImpacts Interactive Web Tool!')
-st.markdown('This is a work in progress. There are errors and unfinished portions for different commodities, but select Maize or Palm Oil to see a (mostly) working product.')
+st.markdown('This is a work in progress. Additional features and polish are on their way!')
 
 @st.cache(show_spinner=False, persist=True)  # save computation between reruns
 def get_full_df():
     with st.spinner('Loading excel spreadsheet...'):  # show a progress meter
-        full_df = pd.read_excel('Terrafrosh_All_Commodity_Data.xlsx', sheet_name=1,
+        full_df = pd.read_excel('AgImpacts_Raw_Data.xlsx', sheet_name=1,
                                 header=1)
     return full_df
 
 def format_col(df, col):
-    df[col] = df[col].replace(',', '', regex=True).replace('-', '', regex=True).replace('%', '', regex=True).replace(' ', '', regex=True).replace('', 'NaN').astype(float)  # remove commas
-    return df[col]
+    del_things = (',', '-', '%', ' ')
+    for del_thing in del_things:
+        df[col] = df[col].replace(del_thing, '', regex=True)
+    df[col] = df[col].replace('', 'NaN').replace('', 'nan').astype(float, errors='ignore')
+    return df[col].dropna()
+
+def format_fig(fig):
+    fig.update_layout(
+        font_family="IBM Plex Sans",
+        font_color="black",
+        title_font_family="IBM Plex Sans",
+        title_font_color="black",
+        legend_title_font_color="black")
+    fig.update_xaxes(title_font_family="IBM Plex Sans")
+    fig.update_yaxes(title_font_family="IBM Plex Sans")
+    return fig
 
 full_df = get_full_df()
 
@@ -33,45 +47,61 @@ commodity = st.sidebar.selectbox(label='Commodity', options=list(commodity_rows.
 start, end = commodity_rows[commodity]
 df_filtered = full_df[start:end].dropna(axis=0, subset=['Reference'])
 
-numerical_cols = ['Land Use (m^2*year)', 'GHG Emissions (kg CO2 eq)', 'Freshwater Withdrawal (L)', 
-'Acidifying Emissions (kg SO2 eq)', 'Eutrophying Emissions (kg PO4 eq)']
+numerical_cols = ['GHG Emissions', 'Land Use', 'Eutrophication Potential', 
+                'Acidification Potential', 'Freshwater Withdrawal']
 
 col_labels = [  # Tuples of (column name, pretty print name for axis labels)
-('Land Use (m^2*year)','Land Use (m<sup>2</sup>*yr)'),
-('Freshwater Withdrawal (L)', 'Freshwater Withdrawal (L)'),
-('Eutrophying Emissions (kg PO4 eq)', 'Eutrophication Potential (kg PO<sub>4</sub><sup>3-</sup> eq)'),
-('Acidifying Emissions (kg SO2 eq)', 'Acidification Potential (kg SO<sub>2</sub> eq)')]
+    ('Land Use','Land Use (m<sup>2</sup>*yr)'),
+    ('Eutrophication Potential', 'Eutrophication Potential (kg PO<sub>4</sub><sup>3-</sup> eq)'),
+    ('Acidification Potential', 'Acidification Potential (kg SO<sub>2</sub> eq)'),
+    ('Freshwater Withdrawal', 'Freshwater Withdrawal (L)')
+    ]
+col_labels_dict = {'GHG Emissions': 'GHG Emissions (kg CO<sub>2</sub> eq)',
+    'Land Use': 'Land Use (m<sup>2</sup>*yr)',
+    'Eutrophication Potential': 'Eutrophication Potential (kg PO<sub>4</sub><sup>3-</sup> eq)',
+    'Acidification Potential': 'Acidification Potential (kg SO<sub>2</sub> eq)',
+    'Freshwater Withdrawal': 'Freshwater Withdrawal (L)'}
 
-indicators = ['Land Use (m^2*year)', 'Freshwater Withdrawal (L)', 
-'Acidifying Emissions (kg SO2 eq)', 'Eutrophying Emissions (kg PO4 eq)']
+indicators = ['Land Use', 'Eutrophication Potential', 
+    'Acidification Potential', 'Freshwater Withdrawal']
 
-ghg = 'GHG Emissions (kg CO2 eq)'
+ghg = 'GHG Emissions'
 
-with st.beta_expander('Indicator Graphs'):
+with st.beta_expander('Indicator Charts'):
+    options = st.selectbox(
+        'Select Chart Features',
+        ['Linear Trendline', 'Non-Linear Trendline',# 'Display Statistics', 'Label by Country', 'Label by System Type'
+        ])
+    trendline_dict = {
+        'Linear Trendline': 'ols',
+        'Non-Linear Trendline': 'lowess',
+        'Display Statistics' : None,
+        'Label by Country' : None, 
+        'Label by System Type' : None
+        }
     for y, name in col_labels:
-        fig = px.scatter(x=df_filtered['GHG Emissions (kg CO2 eq)'],
+        df_filtered[y] = format_col(df_filtered, y)
+        fig = px.scatter(x=df_filtered['GHG Emissions'],
                     template='simple_white',
-            y=df_filtered[y],
+            y=df_filtered[y], color=df_filtered['Country'] if options == 'Label by Country' else None, 
+            symbol = df_filtered['System'] if options == 'Label by System Type' else None,
             title=f'{name} vs. GHG Emissions (kg CO<sub>2</sub> eq)',
-            labels={'x': 'GHG Emissions (kg CO<sub>2</sub> eq)', 'y':name, 'color' : 'System'}#, trendline = 'ols'
+            labels={'x': 'GHG Emissions (kg CO<sub>2</sub> eq)', 'y':name, 'color' : 'Country', 'symbol' : 'System'}, trendline = trendline_dict[options],
             )
-        fig.update_layout(
-        font_family="Calibri",
-        font_color="black",
-        title_font_family="Calibri",
-        title_font_color="black",
-        legend_title_font_color="black")
-        fig.update_xaxes(title_font_family="Calibri")
-        fig.update_yaxes(title_font_family="Calibri")
-        #results = px.get_trendline_results(fig)
-        #results = results.px_fit_results.iloc[0].summary()
+        fig = format_fig(fig)
         st.plotly_chart(fig)
+        if options == 'Display Statistics':
+            results = px.get_trendline_results(fig)
+            st.write(str(results))
 
 with st.beta_expander('Impact Analysis'):
-    cutoff = st.slider('Select a range of GHG emissions', min_value=df_filtered[ghg].min(), max_value=df_filtered[ghg].max())
-    st.markdown(f'Your selected range is from {df_filtered[ghg].min()} (kg CO<sub>2</sub> eq) to {cutoff} (kg CO<sub>2</sub> eq).', unsafe_allow_html=True)
-    cutoff_df = df_filtered.loc[df_filtered[ghg] <= cutoff, [ghg, 'Land Use (m^2*year)', 'Freshwater Withdrawal (L)', 
-    'Acidifying Emissions (kg SO2 eq)', 'Eutrophying Emissions (kg PO4 eq)']]
+    ghg_col = format_col(df_filtered, ghg)
+    df_filtered[ghg] = ghg_col
+    ghg_points = sorted(set(ghg_col))
+    min_cutoff, max_cutoff = st.select_slider('Select a range of GHG emissions', ghg_points, value=(min(ghg_points), max(ghg_points)))
+    st.markdown(f'Your selected range is from {min_cutoff} (kg CO<sub>2</sub> eq) to {max_cutoff} (kg CO<sub>2</sub> eq).', unsafe_allow_html=True)
+    cutoff_df = df_filtered.loc[(df_filtered[ghg] <= max_cutoff) & (df_filtered[ghg] >= min_cutoff), ['GHG Emissions', 'Land Use', 'Eutrophication Potential', 
+    'Acidification Potential', 'Freshwater Withdrawal']]
     show_data = st.checkbox('Show Filtered Raw Data')
     show_quantiles = st.checkbox('Show Quantiles of GHG Emissions')
     if show_data:
@@ -82,46 +112,41 @@ with st.beta_expander('Impact Analysis'):
             st.dataframe(quantile_list)
     for y, name in col_labels:
         cutoff_df[y] = format_col(cutoff_df, y)
-        fig = px.scatter(x=cutoff_df['GHG Emissions (kg CO2 eq)'],
+        fig = px.scatter(x=cutoff_df['GHG Emissions'],
                     template='simple_white',
             y=cutoff_df[y],
             title=f'{name} vs. GHG Emissions (kg CO<sub>2</sub> eq)',
             labels={'x': 'GHG Emissions (kg CO<sub>2</sub> eq)', 'y':name, 'color' : 'System'}#, trendline = 'ols'
             )
-        fig.update_layout(
-        font_family="Calibri",
-        font_color="black",
-        title_font_family="Calibri",
-        title_font_color="black",
-        legend_title_font_color="black")
-        fig.update_xaxes(title_font_family="Calibri")
-        fig.update_yaxes(title_font_family="Calibri")
-        #results = px.get_trendline_results(fig)
-        #results = results.px_fit_results.iloc[0].summary()
+        fig = format_fig(fig)
         st.plotly_chart(fig)
         st.markdown(f'The median {name} for your selected range is {cutoff_df[y].median()}, and the average is {round(cutoff_df[y].mean(), 5)}.', unsafe_allow_html=True)
 
 with st.beta_expander('Geographic Overview of Indicators'):
-    st.markdown('This tool analyzes a column as a function of the country.')
-    col = st.selectbox(label='Column to Analyze', options=numerical_cols)
+    st.markdown('This tool analyzes indicators by geographic region.')
+    col = st.selectbox(label='Indicator to Analyze', options=numerical_cols)
     df_filtered[col] = format_col(df_filtered, col)
     data = df_filtered.groupby('Country')[col].mean()
-
-    fig = px.bar(x=data.index, y=data, title=f'{col} vs. Country for {commodity}',
-                 labels={'x': 'Country', 'y': col}, width=1500, height=600)
-    st.plotly_chart(fig)
-
+    
     fig = px.scatter_geo(size=data.fillna(0), locations=data.index, locationmode='country names',
-                         title=f'{col} for {commodity}', labels={'locations': 'Country', 'size': col}, width=1500,
-                         height=600)
+                         title=f'Global {col_labels_dict[col]} for {commodity}', labels={'locations': 'Country', 'size': col}, 
+                        #  width=1500, height=600
+                        template='simple_white')
+    fig = format_fig(fig)
+    st.plotly_chart(fig)
+    fig = px.bar(x=data.index, y=data.fillna(0), title=f'{col_labels_dict[col]} vs. Country for {commodity}',
+                 labels={'x': 'Country', 'y': col_labels_dict[col]}, 
+                #  width=1500, height=600, 
+                template='simple_white')
+    fig = format_fig(fig)
     st.plotly_chart(fig)
 
 with st.beta_expander('See Raw Data for Commodity'):
-    st.markdown('[See the paper with original dataset here.](https://science.sciencemag.org/content/360/6392/987)', unsafe_allow_html=True)
+    st.markdown('[See the original data sources here.](https://github.com/anushreechaudhuri/agimpacts/blob/master/AgImpacts_Data_Sources.md)', unsafe_allow_html=True)
     st.dataframe(df_filtered)
 
 
-##BAR GRAPHS BASED ON AVERAGES -- WASN'T A GOOD IDEA
+##OLD CODE FOR BAR GRAPHS BASED ON AVERAGES -- WASN'T A GOOD IDEA
     # q1 = st.checkbox('Quartile 1 of Producer GHG Emissions (0-25%)')
     # q2 = st.checkbox('Quartile 2 of Producer GHG Emissions (25-50%)')
     # q3 = st.checkbox('Quartile 3 of Producer GHG Emissions (50-75%)')
